c058d3427edd61bf33a9c9000dbfed2bbba06659
Igor Knyazev <knyazev@artw.ru>
2023-04-13 12:53:02 +0200

    initial commit

---
+++ precache-manifest.672fe52e730f61983dc98cfaddfe8c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64772fd36eb8de152eeee90516308cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869545d773967f004fcd",
+    "url": "/static/css/main.9ee91a79.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869545d773967f004fcd",
+    "url": "/static/js/main.703c16e5.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
